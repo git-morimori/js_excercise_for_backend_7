ed512a07b78bd128bb4d7c739e511066052a27a0
haruka-g <mortndpgj@gmail.com>
2019-12-04 23:31:43 +0900

    apiのテストを実装、app.jsの記述の順番を変更

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = require('./routers/comment');
 const app = express();
 
-app.use('/api/comments', router);
 //json形式で送信されたデータをサーバー内で「req.body」で操作できるようにする
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//上記の記述の前に以下を記述するとresponseが意図せぬものが返ってくる
+app.use('/api/comments', router);
 
 module.exports = app;
