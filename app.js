e460d4a6c5453618a9c4e73b3668907ea9219bac
haruka-g <mortndpgj@gmail.com>
2019-12-04 21:42:22 +0900

    req.bodyでデータが取得できるように設定

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const router = require('./routers/comment');
 const app = express();
 
 app.use('/api/comments', router);
+//json形式で送信されたデータをサーバー内で「req.body」で操作できるようにする
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
